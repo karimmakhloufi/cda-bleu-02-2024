facfd792157a18ba4b57b3b4b6c08fcd2d0baefa
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-02-27 10:31:05 +0100

    GET and POST ads on backend for tgc

--- the-good-corner/backend/src/index.ts
+++ the-good-corner/backend/src/index.ts
@@ -1,10 +1,23 @@
 import express from "express";
+import { ads } from "./data";
 
 const app = express();
+app.use(express.json());
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World on port 5000!");
+});
+
+app.get("/ads", (req, res) => {
+  res.send(ads);
+});
+
+app.post("/ads", (req, res) => {
+  console.log("req body", req.body);
+  ads.push(req.body);
+  console.log("test");
+  res.send("Ad has been added");
 });
 
 app.listen(port, () => {
