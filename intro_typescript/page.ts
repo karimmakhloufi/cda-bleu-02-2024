e57197c63fd43f085eddc373a60812f0b6eb6767
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-02-27 09:06:19 +0100

    Added Typescript examples

--- intro_typescript/page.ts
+++ intro_typescript/page.ts
@@ -1,3 +1,15 @@
 import { prettyDate } from "./utils";
-
+import { User } from "./user";
 console.log(prettyDate(new Date()));
+
+const getFromApi = (): any => {
+  return {
+    user: {
+      name: "Alice",
+    },
+  };
+};
+
+const u2: User = { location: "Paris", name: "Karim" };
+
+const data = getFromApi();
---
+++ intro_typescript/user.ts
@@ -0,0 +1,2 @@
+export type User = { name: string; location: string };
+const u1: User = { name: "Aurélien", location: "Lyon" };
