27523f108ac643d9a892bd85d0e3630d428495a7
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-02-29 10:00:22 +0100

    Added category to tgc

--- the-good-corner/backend/category.sql
+++ the-good-corner/backend/category.sql
@@ -1,18 +1,19 @@
-
+/*
 CREATE TABLE category
 (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name TEXT NOT NULL
 );
-/*
+
 
 
 ALTER TABLE ad
 ADD COLUMN category_id INTEGER REFERENCES category(id);
 
 
+
 INSERT INTO category (name) VALUES ('autre')
+*/
 
 PRAGMA foreign_keys = ON;
-INSERT INTO ad (title, owner, category_id) VALUES ('test false category', 'test', 10);
-*/
+INSERT INTO ad (title, owner, category_id) VALUES ('brouette a vendre', 'vendeur de brouette', 1);
--- the-good-corner/backend/src/index.ts
+++ the-good-corner/backend/src/index.ts
@@ -3,32 +3,7 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database("the_good_corner.sqlite");
 
-let ads = [
-  {
-    id: 1,
-    title: "Bike to sell",
-    description:
-      "My bike is blue, working fine. I'm selling it because I've got a new one",
-    owner: "bike.seller@gmail.com",
-    price: 100,
-    picture:
-      "https://images.lecho.be/view?iid=dc:113129565&context=ONLINE&ratio=16/9&width=640&u=1508242455000",
-    location: "Paris",
-    createdAt: "2023-09-05T10:13:14.755Z",
-  },
-  {
-    id: 2,
-    title: "Car to sell",
-    description:
-      "My car is blue, working fine. I'm selling it because I've got a new one",
-    owner: "car.seller@gmail.com",
-    price: 10000,
-    picture:
-      "https://www.automobile-magazine.fr/asset/cms/34973/config/28294/apres-plusieurs-prototypes-la-bollore-bluecar-a-fini-par-devoiler-sa-version-definitive.jpg",
-    location: "Paris",
-    createdAt: "2023-10-05T10:14:15.922Z",
-  },
-];
+db.get("PRAGMA foreign_keys = ON");
 
 const app = express();
 app.use(express.json());
@@ -45,18 +20,28 @@ app.get("/ads", (req, res) => {
 });
 
 app.post("/ads", (req, res) => {
-  console.log("req body", req.body);
+  // console.log("req body", req.body);
   const stmt = db.prepare(
-    "INSERT INTO ad (title, description, owner, price, ville ) VALUES (?,?,?,?,?)"
+    "INSERT INTO ad (title, description, owner, price, ville, category_id ) VALUES (?,?,?,?,?,?)"
   );
-  stmt.run([
-    req.body.title,
-    req.body.description,
-    req.body.owner,
-    req.body.price,
-    req.body.location,
-  ]);
-  res.send("Ad has been added");
+  stmt.run(
+    [
+      req.body.title,
+      req.body.description,
+      req.body.owner,
+      req.body.price,
+      req.body.location,
+      req.body.categoryId,
+    ],
+    (err) => {
+      if (err) {
+        console.log("error");
+        res.status(500).send("An error has occured");
+      } else {
+        res.status(201).send("Ad has been added");
+      }
+    }
+  );
 });
 
 app.delete("/ads/:idToDelete", (req, res) => {
