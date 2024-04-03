c0f1eb64eac88be165cff778a79278ce6777ffa5
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-04-03 14:25:53 +0200

    Added mutation to graphql

--- the-good-corner/backend/src/resolvers/AdResolver.ts
+++ the-good-corner/backend/src/resolvers/AdResolver.ts
@@ -1,5 +1,23 @@
 import { Ad } from "../entities/ad";
-import { Query, Resolver } from "type-graphql";
+import { Arg, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
+
+@InputType()
+class NewAdInput {
+  @Field()
+  title: string;
+
+  @Field()
+  description: string;
+
+  @Field()
+  owner: string;
+
+  @Field()
+  price: number;
+
+  @Field()
+  ville: string;
+}
 
 @Resolver(Ad)
 class AdResolver {
@@ -7,6 +25,13 @@ class AdResolver {
   async getAllAds() {
     const ads = await Ad.find();
     return ads;
+  }
+
+  @Mutation(() => Ad)
+  async createNewAd(@Arg("data") newAdData: NewAdInput) {
+    console.log("new ad data", newAdData);
+    const result = await Ad.save({ ...newAdData });
+    return result;
   }
 }
 
