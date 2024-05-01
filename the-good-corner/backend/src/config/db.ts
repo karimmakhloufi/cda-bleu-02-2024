efc25a2dfff935e6f777a466170f8845f419ee85
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-05-01 20:14:56 +0200

    postgres as db with docker

--- the-good-corner/backend/src/config/db.ts
+++ the-good-corner/backend/src/config/db.ts
@@ -4,9 +4,13 @@ import { DataSource } from "typeorm";
 import { Tag } from "../entities/tag";
 
 export const dataSource = new DataSource({
-  type: "sqlite",
-  database: "db.sqlite",
-  synchronize: true,
-  logging: ["error", "query"],
+  type: "postgres",
+  host: "db",
+  port: 5432,
+  username: "postgres",
+  password: "example",
+  database: "postgres",
   entities: [Ad, Category, Tag],
+  synchronize: true,
+  logging: ["query", "error"],
 });
