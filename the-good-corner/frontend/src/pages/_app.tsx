908b92411d5b2b8fcc081d5b0202de876eed4cba
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-05-02 16:34:17 +0200

    Added frontend on api gateway

--- the-good-corner/frontend/src/pages/_app.tsx
+++ the-good-corner/frontend/src/pages/_app.tsx
@@ -5,7 +5,7 @@ import dynamic from "next/dynamic";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: "/api",
   cache: new InMemoryCache(),
 });
 
