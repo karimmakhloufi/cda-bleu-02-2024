1bccfb2cc7203fea10fe74f71244aefdfbf7b5af
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-05-02 10:39:53 +0200

    Added postgres and adminer, removed sqlite

--- the-good-corner/backend/src/config/db.ts
+++ the-good-corner/backend/src/config/db.ts
@@ -4,8 +4,12 @@ import { DataSource } from "typeorm";
 import { Tag } from "../entities/tag";
 
 export const dataSource = new DataSource({
-  type: "sqlite",
-  database: "db.sqlite",
+  type: "postgres",
+  host: "db",
+  port: 5432,
+  username: "postgres",
+  password: "example",
+  database: "postgres",
   synchronize: true,
   logging: ["error", "query"],
   entities: [Ad, Category, Tag],
