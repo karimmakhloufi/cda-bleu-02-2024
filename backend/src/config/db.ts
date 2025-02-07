463510d148e7cdf3d65c0b102df143fffc144ef7
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2025-02-07 17:47:00 +0100

    Postgres replica

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -6,12 +6,26 @@ import { User } from "../entities/user";
 
 export const dataSource = new DataSource({
   type: "postgres",
-  host: "db",
-  port: 5432,
-  username: "postgres",
-  password: "example",
-  database: "postgres",
   synchronize: true,
   logging: ["error", "query"],
   entities: [Ad, Category, Tag, User],
+
+  replication: {
+    master: {
+      host: "db",
+      port: 5432,
+      username: "postgres",
+      password: "my_password",
+      database: "postgres",
+    },
+    slaves: [
+      {
+        host: "db-replica",
+        port: 5432,
+        username: "postgres",
+        password: "my_password",
+        database: "postgres",
+      },
+    ],
+  },
 });
